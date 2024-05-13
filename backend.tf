45adcce17a8bd69630843be61b79ecf177ddeb72
jaideep <jaideepdoshi86@example.com>
2024-05-13 17:41:50 +0530

    Bucket name chnage

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   
   backend "s3" {
-  bucket = "mydev-tf-state-bucket-project-terraform-batch-23"
+  bucket = "mydev-tf-state-bucket-project-terraform-batch-25"
   key="aws"
   region = "ap-south-1"
   dynamodb_table = "my-dynamodb-table"
